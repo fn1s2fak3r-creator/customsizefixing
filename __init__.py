a8867d16a978e923f31c39eb19fb87036e85db64
fn1s2fak3r-creator <fn1s2fak3r@gmail.com>
2025-11-29 10:32:10 +0900

    init

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for customsizefixing."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """"""
+__email__ = "you@gmail.com"
+__version__ = "0.0.1"
+
+from .src.customsizefixing.nodes import NODE_CLASS_MAPPINGS
+from .src.customsizefixing.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
